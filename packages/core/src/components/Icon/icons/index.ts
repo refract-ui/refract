ff6a1d3dc6c3e9d05b2b32d22ccce60591f0cf3b
alex christie <alexj.christie@gmail.com>
2020-08-07 11:05:01 -0500

    [wip] icon -- monochromatic

--- packages/core/src/components/Icon/icons/index.ts
+++ packages/core/src/components/Icon/icons/index.ts
@@ -1,24 +1,92 @@
-import { IconTree } from 'react-icons';
-import * as Md from './md';
-import * as Sm from './sm';
-import * as Lg from './lg';
+export interface IconTree {
+  paths: string[];
+  attrs?: {
+    [key: string]: string | number;
+  }[];
+  isMulticolor?: boolean;
+  isMulticolor2?: boolean;
+  grid: number;
+  tags: string[];
+}
+
+export interface IconObject {
+  metadata: {
+    name: string;
+    importSize: {
+      width: number;
+      height: number;
+    };
+  };
+  icons: IconTree[];
+}
 
 export type Icons = {
-  add: IconTree;
-  back?: IconTree;
+  Add: IconTree;
+  Back: IconTree;
+  Behance: IconTree;
+  Blank: IconTree;
+  Calendar: IconTree;
+  Camera: IconTree;
+  Check: IconTree;
+  Clock: IconTree;
+  Close: IconTree;
+  CloseRound: IconTree;
+  Copy: IconTree;
+  Danger: IconTree;
+  Dashboard: IconTree;
+  Delete: IconTree;
+  Document: IconTree;
+  Dollar: IconTree;
+  DotsHorizontal: IconTree;
+  DotsVertical: IconTree;
+  Download: IconTree;
+  Dribbble: IconTree;
+  Edit: IconTree;
+  Expand: IconTree;
+  Eye: IconTree;
+  Facebook: IconTree;
+  Favorite: IconTree;
+  Filter: IconTree;
+  Forward: IconTree;
+  Heading: IconTree;
+  Image: IconTree;
+  Info: IconTree;
+  Instagram: IconTree;
+  LineGrid: IconTree;
+  Link: IconTree;
+  LinkedIn: IconTree;
+  List: IconTree;
+  LoadingSpinner: IconTree;
+  Location: IconTree;
+  Lock: IconTree;
+  Mail: IconTree;
+  Map: IconTree;
+  Medium: IconTree;
+  Menu: IconTree;
+  Notification: IconTree;
+  Person: IconTree;
+  Phone: IconTree;
+  Print: IconTree;
+  Refresh: IconTree;
+  RoundArrow: IconTree;
+  Save: IconTree;
+  Search: IconTree;
+  Settings: IconTree;
+  Shrink: IconTree;
+  SimpleArrowDown: IconTree;
+  SimpleArrowUp: IconTree;
+  Skype: IconTree;
+  Slack: IconTree;
+  Star: IconTree;
+  Table: IconTree;
+  Text: IconTree;
+  Twitter: IconTree;
+  Upload: IconTree;
+  Video: IconTree;
+  Vimeo: IconTree;
+  Website: IconTree;
+  Whatsapp: IconTree;
+  YoutubeBlack: IconTree;
 };
 
-export const smallIcons: Icons = {
-  add: Sm.AddIcon
-};
-
-export const defaultIcons: Icons = {
-  add: Md.AddIcon,
-  back: Md.BackIcon
-};
-
-export const largeIcons: Icons = {
-  add: Lg.AddIcon
-};
-
-export default defaultIcons;
+export default Icons;
