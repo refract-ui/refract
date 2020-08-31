02a31b8a0586d8e09f27b4b8162ead7be66e7f14
Brett Cimbalik <bcimbali@gmail.com>
2020-08-31 15:40:27 -0500

    [fix] use filled as variant in textinput

--- packages/core/src/components/Text-Input/index.tsx
+++ packages/core/src/components/Text-Input/index.tsx
@@ -28,7 +28,6 @@ type TextInputProps = {
 };
 
 type TextInputVariants = {
-  color: keyof Colors | keyof ThemeColors;
   size?: 'sm' | 'md';
   filled?: boolean;
 };
@@ -40,7 +39,6 @@ function TextInputFunction({
   value,
   ...props
 }: TextInputProps & TextInputVariants): JSX.Element {
-  console.log('props', props);
   const className = get(props, 'className', null);
   return (
     <input className={className} placeholder={placeholder} value={value} />
@@ -50,10 +48,7 @@ function TextInputFunction({
 const TextInputComponent = styled(TextInputFunction)<
   ThemeComponent & TextInputProps
 >`
-  ${({ componentCss }) => {
-    console.log('componentCss', componentCss);
-    return componentCss;
-  }};
+  ${({ componentCss }) => componentCss};
   font-family: 'Work Sans', sans serif;
   font-weight: 300;
 `;
@@ -66,112 +61,110 @@ const TextInput = createThemedComponent<
   Container
 >({
   defaultVariants: {
-    color: 'white',
     size: 'md',
     filled: false
   },
   states: ['_hover', '_active', '_focus'],
   extend: mapDivContainerPropsToStyles,
-  compose: ({ theme, variant }) => ({
-    Component: TextInputComponent,
+  compose: ({ theme, variant }) => {
+    console.log('In index.tsx, this is variant: ', variant);
+    console.log('In index.tsx, this is theme: ', theme);
 
-    variantMapping: {
-      color: ({ color, filled }) => {
-        if (!filled) {
-          return {
-            backgroundColor: theme['white'],
-            border: {
-              borderColor: theme['secondary'],
-              borderRadius: '8px'
-            }
-          };
-        }
-        if (filled) {
-          return {
-            backgroundColor: theme['gray300'],
-            border: {
-              borderColor: 'transparent',
-              borderRadius: '8px'
-            }
-          };
+    return {
+      Component: TextInputComponent,
+
+      variantMapping: {
+        filled: ({ filled }) => {
+          if (filled) {
+            return {
+              backgroundColor: theme['gray300'],
+              border: {
+                borderColor: 'transparent'
+              }
+            };
+          } else {
+            return {
+              backgroundColor: 'transparent',
+              textColor: theme['dark']
+            };
+          }
+        },
+        size: ({ size }) => {
+          if (size === 'sm') {
+            return {
+              py: `${theme.spacing['0']}`
+            };
+          } else {
+            return {
+              py: `${theme.spacing['1']}`
+            };
+          }
         }
       },
-      size: ({ size }) => {
-        if (size === 'sm') {
-          return {
-            py: `${theme.spacing['0']}`
-          };
-        } else {
-          return {
-            py: `${theme.spacing['1']}`
-          };
+
+      defaultStyleMapping: {
+        xs: {
+          backgroundColor: '',
+          textColor: ({ contrastColor, backgroundColor }) =>
+            contrastColor(backgroundColor),
+          height: '42px',
+          border: theme.borders.md,
+          px: `${theme.spacing['3']}`,
+          py: `0`,
+          width: '100%'
+        },
+
+        sm: {
+          width: '320px'
+        },
+
+        md: {
+          px: `${theme.spacing['4']}`,
+          width: '320px'
         }
+      },
+
+      // hover, active state handling here:
+      cascadeStateProps: {},
+
+      mapPropsToStyle: {
+        backgroundColor: ({ backgroundColor }) => css`
+          background-color: ${backgroundColor};
+
+          :focus,
+          :active {
+            outline: none;
+          }
+        `,
+        textColor: ({ textColor }) => css`
+          color: ${textColor};
+          font-size: 1rem;
+          line-height: 19px;
+        `,
+        height: ({ height }) => css`
+          height: ${height};
+        `,
+        border: props =>
+          applyBorderStyle({
+            borderColor: props.bg,
+            ...props.border
+          }),
+        px: ({ px }) => css`
+          padding-left: ${px};
+          padding-right: ${px};
+        `,
+        py: ({ py, ...props }) => {
+          return css`
+            padding-top: ${py};
+            padding-bottom: ${py};
+          `;
+        },
+        width: ({ width }) => css`
+          width: ${width};
+        `
       }
-    },
-
-    defaultStyleMapping: {
-      xs: {
-        backgroundColor: theme[variant.color],
-        textColor: ({ contrastColor, backgroundColor }) =>
-          contrastColor(backgroundColor),
-        border: theme.borders.md,
-        height: '42px',
-        px: `${theme.spacing['3']}`,
-        py: `0`,
-        width: '100%'
-      },
-
-      sm: {
-        width: '320px'
-      },
-
-      md: {
-        px: `${theme.spacing['4']}`,
-        width: '320px'
-      }
-    },
-
-    cascadeStateProps: {},
-
-    mapPropsToStyle: {
-      backgroundColor: ({ backgroundColor }) => css`
-        background-color: ${backgroundColor};
-
-        :focus,
-        :active {
-          outline: none;
-        }
-      `,
-      textColor: ({ backgroundColor, contrastColor }) => css`
-        color: ${contrastColor(backgroundColor)};
-        font-size: 1rem;
-        line-height: 19px;
-      `,
-      height: ({ height }) => css`
-        height: ${height};
-      `,
-      border: props =>
-        applyBorderStyle({
-          borderColor: props.bg,
-          ...props.border
-        }),
-      px: ({ px }) => css`
-        padding-left: ${px};
-        padding-right: ${px};
-      `,
-      py: ({ py, ...props }) => {
-        console.log('In py, this is props: ', props);
-        console.log('py', py);
-        return css`
-          padding-top: ${py};
-          padding-bottom: ${py};
-        `;
-      },
-      width: ({ width }) => css`
-        width: ${width};
-      `
-    }
-  })
+    };
+  }
 });
 
 export default TextInput;
