cea2893050bb0a5bfdd37a889fcd230bbbe4fb35
Lynn Dylan Hurley <lynn.dylan.hurley@gmail.com>
2021-05-17 01:21:29 -0500

    [wip] rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,44 @@
-import typescript from '@rollup/plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
 import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
+import dts from 'rollup-plugin-dts';
 import { terser } from 'rollup-plugin-terser';
 
-export default {
-  input: 'packages/core/src/index.ts',
-  output: {
-    dir: 'packages/core/dist',
-    format: 'umd',
-    name: '@refract/core'
+export default [
+  {
+    input: 'packages/core/src/index.ts',
+    output: {
+      file: 'packages/core/dist/index.js',
+      format: 'es',
+      name: '@refract/core'
+    },
+    plugins: [
+      typescript({
+        useTsconfigDeclarationDir: true,
+        tsconfigOverride: {
+          compilerOptions: {
+            declarationDir: 'packages/core/dist/.dts'
+          }
+        }
+      }),
+      resolve({
+        // pass custom options to the resolve plugin
+        customResolveOptions: {
+          moduleDirectories: [
+            'node_modules',
+            'packages/core/node_modules',
+            'packages/docs/node_modules'
+          ]
+        }
+      }),
+      babel({ babelHelpers: 'bundled' }),
+      terser()
+    ],
+    external: ['lodash', 'tinycolor2', 'styled-components', 'react']
   },
-  plugins: [
-    typescript(),
-    resolve({
-      // pass custom options to the resolve plugin
-      customResolveOptions: {
-        moduleDirectories: [
-          'node_modules',
-          'packages/core/node_modules',
-          'packages/docs/node_modules'
-        ]
-      }
-    }),
-    babel({ babelHelpers: 'bundled' }),
-    terser()
-  ],
-  external: ['lodash', 'tinycolor2', 'styled-components', 'react']
-};
+  {
+    input: 'packages/core/dist/.dts/packages/core/src/index.d.ts',
+    plugins: [dts()],
+    output: { file: 'packages/core/dist/index.d.ts', format: 'es' }
+  }
+];
