0c70964b6c699e5636aedee95ed969ae0bfee7ed
Lynn Dylan Hurley <lynn.dylan.hurley@gmail.com>
2021-07-14 15:10:53 -0500

    [bugfix] storybook typescript config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,25 +1,7 @@
 module.exports = {
   stories: [
-    //'../packages/**/*.stories.tsx'
-    // '../packages/**/*.stories.ts',
-    // '../packages/core/src/components/Button/Button.stories.tsx',
     '../packages/docs/src/**/*.stories.tsx',
     '../packages/docs/src/**/*.stories.mdx'
   ],
   addons: ['@storybook/addon-essentials', '@storybook/addon-docs'],
-  webpackFinal: async (config: any) => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      use: [
-        {
-          loader: require.resolve('ts-loader')
-        },
-        {
-          loader: require.resolve('react-docgen-typescript-loader')
-        }
-      ]
-    });
-    config.resolve.extensions.push('.ts', '.tsx');
-    return config;
-  }
 };
