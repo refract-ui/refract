806f6ea490e2d15a7e85cd8c9bb66522d21c92ff
alex christie <alexj.christie@gmail.com>
2021-09-27 12:54:37 -0500

    [feat] add ability to pass global styles to refract preset

--- packages/docs/src/preset/decorators/withRefract.tsx
+++ packages/docs/src/preset/decorators/withRefract.tsx
@@ -2,11 +2,14 @@ import * as React from 'react';
 import { StoryContext } from '@storybook/addons';
 // import { ComponentStory, Story } from '@storybook/react';
 import { ThemeProvider } from 'styled-components';
-import { Theme, theme as genTheme } from '@refract-ui/core';
+import { Theme, theme as genTheme, GlobalStyles } from '@refract-ui/core';
+import { ThemeComponent } from '@refract-ui/core/src/theme';
 
 export type RefractDecoratorTheme = {
   theme: Theme;
   name: string;
+  // export the GlobalStyles type from refract
+  ThemeStyle?: React.FC<ThemeComponent>;
 };
 
 export const defaultRefractTheme: RefractDecoratorTheme = {
@@ -18,13 +21,15 @@ export function withRefract(Story: any, c: StoryContext): any {
   const themes = c.parameters?.refract?.themes || [];
 
   const decoratorThemes = [defaultRefractTheme, ...themes];
-  const { theme } =
+  const { theme, ThemeStyle } =
     themes.length > 0 && c.globals.refractTheme
       ? decoratorThemes.find(({ name }) => name === c.globals.refractTheme)
       : decoratorThemes[0];
 
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      {ThemeStyle && <ThemeStyle />}
       <Story {...c} />
     </ThemeProvider>
   );
