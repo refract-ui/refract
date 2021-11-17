f8da8b5687a394f78de210ca4fde020d717f89ec
Jonathan Reyes <150944+panbanda@users.noreply.github.com>
2021-11-17 11:14:11 +0200

    Optimize repo (#13)
    * feat: added workflows and changeset updates
    
    * fix: workflow on wrong branch
    
    * fix: added lerna deploy workflow
    
    * fix: added eslint dist ignore
    
    * feat: added crossenv and skip husky for release
    
    * fix: checking only storybook and packages
    
    * fix: removed outdated yarn
    
    * fix: removed yarn ignores
    
    * fix: added commitlint
    
    * fix: added skip to other husky file
    
    * Publish
    
     - @refract-ui/core@0.0.9
     - @refract-ui/docs@0.0.4
    
    * Revert "fix: removed outdated yarn"
    
    This reverts commit 281e32adbc4a7dac8ebb21018419984bd7a46c18.
    
    * Revert "fix: removed yarn ignores"
    
    This reverts commit 49e07cf9390111f6df0e2624144943dd2127a273.
    
    * feat: upgraded to yarn3

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  ignorePatterns: ['**/dist/**'],
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   parserOptions: {
     ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
