7b4a87fdee7d3e9c16f9e13673fff02468556892
Jonathan Reyes <me@jonathanreyes.com>
2022-01-04 07:49:02 -0500

    fix: added some more storybook plugins and webpack

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,9 +1,15 @@
 module.exports = {
   stories: ['../packages/refract-ui-docs/**/*.stories.@(tsx|mdx)'],
   addons: [
-    '@storybook/addon-essentials',
     '@storybook/addon-docs',
+    '@storybook/addon-essentials',
     '@storybook/addon-links',
     '../packages/refract-ui-docs/preset'
-  ]
+  ],
+  features: {
+    babelModeV7: true
+  },
+  core: {
+    builder: 'webpack5'
+  }
 };
