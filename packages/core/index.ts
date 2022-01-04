3348d15c72da8ab27385520ba303292ce07157db
Jonathan Reyes <150944+panbanda@users.noreply.github.com>
2022-01-04 13:16:06 -0500

    fix: removing comments

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,22 +1,3 @@
-// components
-//export { default as Button } from './components/Button';
-//export { default as TextInput } from './components/Text-Input';
-//export { default as InputWrapper } from './components/Input-Wrapper';
-//export { default as InputLabel } from './components/Input-Label';
-//export { default as InputHelpText } from './components/Input-HelpText';
-//export { default as InputValidationMessage } from './components/Input-ValidationMessage';
-//export { default as InputGroup } from './components/Input-Group';
-//export { default as InputAddon } from './components/Input-Addon';
-//export { default as Dropdown } from './components/Dropdown';
-//export { default as DropdownListWrapper } from './components/Dropdown-List-Wrapper';
-//export { default as DropdownItem } from './components/Dropdown-Item';
-//export { default as DropdownDivider } from './components/Dropdown-Divider';
-//export { default as DropdownGroup } from './components/Dropdown-Group';
-//export { default as DropdownList } from './components/Dropdown-List';
-//export { default as CollapsingList } from './components/Collapsing-List';
-//export { default as CollapsingListContainer } from './components/Collapsing-List-Container';
-//export { default as Checkbox } from './components/Checkbox';
-
 // theme
 export { default as theme, CoreTheme as Theme } from './src/theme';
 
